525e86d1a31b4828bc832b3a2537c2829120e3f8
lara4224 <s1195279@student.windesheim.nl>
2025-10-08 16:13:44 +0200

    UC12: added category

---
+++ Grocery.Core.Data/Repositories/ProductCategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grocery.Core.Interfaces.Repositories;
+
+
+namespace Grocery.Core.Data.Repositories
+{
+    internal class ProductCategoryRepository : IProductCategoryRepository
+    {
+
+    }
+}
