3e418e6ec61d7dc673903431b5efe09993dcd171
lara4224 <s1195279@student.windesheim.nl>
2025-10-08 17:12:30 +0200

    UC12: added category

---
+++ Grocery.App/Views/ProductCategoriesView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grocery.App.ViewModels;
+
+namespace Grocery.App.Views
+{
+    public partial class ProductCategoriesView : ContentPage
+    {
+        public ProductCategoriesView(ProductCategoriesViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
