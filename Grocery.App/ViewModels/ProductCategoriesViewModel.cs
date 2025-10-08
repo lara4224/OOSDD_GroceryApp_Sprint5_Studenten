45a9caf2ec4fc39d0c5007f7f66586435e64cdc2
lara4224 <s1195279@student.windesheim.nl>
2025-10-08 17:06:54 +0200

    UC12: Added category

---
+++ Grocery.App/ViewModels/ProductCategoriesViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using System.Collections.ObjectModel;
+
+namespace Grocery.App.ViewModels
+{
+    [QueryProperty(nameof(Category), nameof(Category))]
+    public partial class ProductCategoriesViewModel : BaseViewModel
+    {
+        private readonly IProductCategoryService _productCategoryService;
+        public ObservableCollection<ProductCategory> ProductCategories { get; set; } = [];
+
+        [ObservableProperty]
+        private Category category = new(0, "None");
+
+        public ProductCategoriesViewModel(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
+        public void LoadProductCategoriesByCategory(Category category)
+        {
+            ProductCategories.Clear();
+
+            var productCategories = _productCategoryService.GetAllOnCategoryId(category.Id);
+            foreach (var productCategory in productCategories)
+                ProductCategories.Add(productCategory);
+        }
+        partial void OnCategoryChanged(Category value)
+        {
+            LoadProductCategoriesByCategory(value);
+        }
+    }
+}
