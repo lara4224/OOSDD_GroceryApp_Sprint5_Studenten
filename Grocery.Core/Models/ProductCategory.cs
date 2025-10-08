785c80235b5d051f107eb2090a4c8788cc84ccae
lara4224 <s1195279@student.windesheim.nl>
2025-10-08 16:05:41 +0200

    USC12: added category

---
+++ Grocery.Core/Models/ProductCategory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Core.Models
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+
+        public ProductCategory(int id, string name, int productId, int categoryId)
+        {
+            Id = id;
+            Name = name;
+            ProductId = productId;
+            CategoryId = categoryId;
+        }
+    }
+}
