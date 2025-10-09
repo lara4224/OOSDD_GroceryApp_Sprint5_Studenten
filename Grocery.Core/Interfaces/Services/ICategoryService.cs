7220fade965cf14797fa0019c5a30c94dda0f0ca
lara4224 <s1195279@student.windesheim.nl>
2025-10-09 11:10:09 +0200

    UC12: added category

--- Grocery.Core/Interfaces/Services/ICategoryService.cs
+++ Grocery.Core/Interfaces/Services/ICategoryService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Grocery.Core.Models;
 
 namespace Grocery.Core.Interfaces.Services
 {
     public interface ICategoryService
     {
-
+        List<Category> GetAll();
     }
 }
